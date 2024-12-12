9c5ec395e19614401343530b3611025a900053ed
achintyakaushaljha <achintya.kaushaljha@infra.market>
2024-12-12 23:44:34 +0530

    dropdowns and parent child relationship

--- src/test/java/BrowserNavigation.java
+++ src/test/java/BrowserNavigation.java
@@ -1,5 +1,9 @@
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WindowType;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.ArrayList;
 
 public class BrowserNavigation {
     public static void main(String args[]){
@@ -8,6 +12,14 @@ public class BrowserNavigation {
         driver.get("https://prr-qa.inframarket.cloud/");
         driver.navigate().to("https://groww.in/login/enter");
         driver.navigate().back();
+        ((JavascriptExecutor) driver).executeScript("window.open();");
+
+        // Switch to the new tab
+        for (String handle : driver.getWindowHandles()) {
+            driver.switchTo().window(handle);
+            //driver.get("https://www.google.com");
+        }
+
         driver.quit();
     }
 }
